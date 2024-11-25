f453f81e5e3df84bfb3b4994f91d438a934c5b15
Anastasiya Burak <anastasiya.burak@innowise.com>
2024-11-25 12:04:58 +0100

    Users List Screen created

--- TechnicalTask-lvl1/Application/SceneDelegate.swift
+++ TechnicalTask-lvl1/Application/SceneDelegate.swift
@@ -1,16 +1,17 @@
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UserViewController()
+        let userViewModel = UserListViewModel()
+        let userListViewController = UserListViewController(viewModel: userViewModel)
+        window?.rootViewController = UINavigationController(rootViewController: userListViewController)
         window?.makeKeyAndVisible()
     }
 }
-
